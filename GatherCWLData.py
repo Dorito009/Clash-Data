4b6d493fd353f3f6c79ef6e33423af514d531742
Dorus van den Broek <65658616+Dorito009@users.noreply.github.com>
2025-05-06 15:43:10 +0200

    Update to save all the rounds GatherCWLData.py

--- GatherCWLData.py
+++ GatherCWLData.py
@@ -20,20 +20,43 @@ rounds = data.get("rounds")
 data.pop("rounds")
 write_data = data
 
-write_data["Tags"] = []
+write_data["tags"] = []
+write_data["clanWars"] = []
+
 write_data["rounds"] = []
-if rounds:
+
+if data.get("state") in (["inWar", "preparation", "warEnded"]):
     for rnd in rounds:
+        round_array = []
         for tag in rnd.get('warTags'):
             if tag != "#0":
                 tag_enc = tag.replace("#", "%23")
                 war_data = requests.get(warurl + tag_enc, headers=headers).json()
                 if war_data.get("clan").get("tag") == clan_tag or war_data.get("opponent").get("tag") == clan_tag:
-                    write_data["Tags"].append(tag)
-                    write_data["rounds"].append(war_data)
-
-
-
+                    write_data["tags"].append(tag)
+                    write_data["clanWars"].append(war_data)
+                if war_data.get("state") in (["inWar", "preparation", "warEnded"]):
+                    round_data = {
+                        "warTag": tag,
+                        "clan1": {
+                            "tag": war_data.get("clan").get("tag"),
+                            "name": war_data.get("clan").get("name"),
+                            "badgeUrls": war_data.get("clan").get("badgeUrls"),
+                            "stars": war_data.get("clan").get("stars"),
+                            "attacks": war_data.get("clan").get("attacks"),
+                            "destructionPercentage": war_data.get("clan").get("destructionPercentage"),
+                        },
+                        "clan2": {
+                            "tag": war_data.get("opponent").get("tag"),
+                            "name": war_data.get("opponent").get("name"),
+                            "badgeUrls": war_data.get("opponent").get("badgeUrls"),
+                            "stars": war_data.get("opponent").get("stars"),
+                            "attacks": war_data.get("opponent").get("attacks"),
+                            "destructionPercentage": war_data.get("opponent").get("destructionPercentage"),
+                        }
+                    }
+                    round_array.append(round_data)
+        write_data["rounds"].append(round_array)
 
 
 with open(f"data/cwl/cwl-{data.get("season")}.json", "w") as f:
